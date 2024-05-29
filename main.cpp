856e9d379fb72d79b06dc27bc22c30dadc3744bf
Rasim <90181726+rasimishere@users.noreply.github.com>
2024-05-29 19:37:11 +0200

    Added main function to create buildings and add residents

--- main.cpp
+++ main.cpp
@@ -14,3 +14,17 @@ struct Building {
     string address;
     vector<Resident> residents;
 };
+
+int main() {
+    Building building1;
+    building1.address = "RasimHolding";
+    building1.residents.push_back({ 101, 2, 1000.00 });
+    building1.residents.push_back({ 111, 1, 800.00 });
+
+    Building building2;
+    building2.address = "RasimHolding2";
+    building2.residents.push_back({ 201, 3, 1900.00 });
+    building2.residents.push_back({ 222, 2, 1100.00 });
+
+    return 0;
+}
